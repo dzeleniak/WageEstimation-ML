744848f95241e2a8013d480a231c0df4435c119a
David Zeleniak <davidzeleniak@Davids-MacBook-Pro.local>
2022-04-18 11:12:49 -0700

    Fixed dataset and added additional lib for cross validation

--- WageEstimation.py
+++ WageEstimation.py
@@ -1,6 +1,14 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
+# Data import
 data = pd.read_csv("adult_data.csv")
 data = pd.DataFrame(data)
-data.info()
+
+# Data separation
+y = data.salary
+X = data.drop(columns=['salary'])
+
+# CV Splitting
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
